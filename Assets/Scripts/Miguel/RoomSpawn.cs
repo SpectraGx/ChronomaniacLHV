5db45407656b325d6bf7226fe3c611e25902c3bc
miguel13241 <123994008+miguel13241@users.noreply.github.com>
2023-09-12 12:15:05 -0600

    sdsdfg

--- Assets/Scripts/Miguel/RoomSpawn.cs
+++ Assets/Scripts/Miguel/RoomSpawn.cs
@@ -4,15 +4,66 @@ using UnityEngine;
 
 public class RoomSpawn : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public int openSide;
+
+    //1 need bottom door
+    //2 need topp door
+    //3 need left door
+    //4 need right dorr 
+
+
+    private RoomTemplates templates;
+    private int rand;
+    private bool spawned = false;
+
     void Start()
     {
-        
+        templates=GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        Invoke("Spawn",0.1f);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void Spawnn()
     {
-        
+        if(spawned==false)
+        {
+             if (openSide==1)
+            {
+                rand=Random.Range(0, templates.bottomRooms.Length);
+                Instantiate(templates.bottomRooms[rand],transform.position, templates.bottomRooms[rand].transform.rotation);
+            }
+            else if(openSide==2)
+            {
+                rand=Random.Range(0, templates.topRooms.Length);
+                Instantiate(templates.topRooms[rand],transform.position, templates.topRooms[rand].transform.rotation);
+            }
+            else if(openSide==3)
+            {
+                rand=Random.Range(0, templates.leftRooms.Length);
+                Instantiate(templates.leftRooms[rand],transform.position, templates.leftRooms[rand].transform.rotation);
+            }
+            else if(openSide==4)
+            {
+                rand=Random.Range(0, templates.rightRooms.Length);
+                Instantiate(templates.rightRooms[rand],transform.position, templates.rightRooms[rand].transform.rotation);
+            }
+            spawned = true;
+        }
+
+       
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+       if (other.CompareTag("SpawnPoint"))
+       {
+        if(other.GetComponent<RoomSpawn>().spawned==false && spawned==false )
+        {
+            Instantiate(templates.closedRoom,transform.position,Quaternion.identity);
+            Destroy(gameObject);
+        }
+            spawned = true;
+       }
     }
 }
