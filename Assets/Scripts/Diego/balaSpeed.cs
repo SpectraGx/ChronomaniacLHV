adc5dd2ed79ab4d08e8155cf7fa52ac258f2643a
diegoalf1710 <124203169+diegoalf1710@users.noreply.github.com>
2023-11-29 11:54:36 -0600

    Movimiento de la bala

---
+++ Assets/Scripts/Diego/balaSpeed.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class balaSpeed : MonoBehaviour
+{
+    public float pistolSpeed;
+    public GameObject projectilePistol;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * pistolSpeed * Time.deltaTime);
+        Destroy(projectilePistol, 1.5f);
+    }
+}
