ee8b644e46572a3795f561e4103ee5f03d9499fb
diegoalf1710 <124203169+diegoalf1710@users.noreply.github.com>
2023-11-29 00:55:26 -0600

    Vida del player y su barra (falta game over para poder reiniciar)

--- Assets/Scripts/Diego/MovFer.cs
+++ Assets/Scripts/Diego/MovFer.cs
@@ -7,20 +7,6 @@ public class MoveFer : MonoBehaviour
     public Weapons currentWeapon;
     public float speedMove = 5f;
     public Transform weapon;
-    public int maxHealth = 100;
-    private int currentHealth;
-    private Vector3 initialPosition;
-    public float knockbackForce = 10f;
-    private Rigidbody2D rb;
-    private bool isKnockedBack = false;
-
-    private void Start()
-    {
-        currentHealth = maxHealth;
-        initialPosition = transform.position;
-        rb = GetComponent<Rigidbody2D>();
-        initialPosition = transform.position;
-    }
 
     private void Update()
     {
@@ -33,52 +19,5 @@ public class MoveFer : MonoBehaviour
         Vector3 displacement = weapon.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
         float angle = Mathf.Atan2(displacement.y, displacement.x) * Mathf.Rad2Deg;
         weapon.rotation = Quaternion.Euler(0f, 0f, angle);
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Enemy"))
-        {
-            if (!isKnockedBack)
-            {
-                TakeHitAndKnockback(collision.contacts[0].point);
-                TakeDamage(20);
-                isKnockedBack = true;
-                Invoke("ResetKnockback", 0.5f); 
-
-                if (currentHealth <= 0)
-                {
-                    Respawn();
-                }
-            }
-        }
-    }
-
-    private void TakeDamage(int damage)
-    {
-        currentHealth -= damage;
-    }
-
-    private void Respawn()
-    {
-        transform.position = initialPosition;
-        currentHealth = maxHealth;
-    }
-
-    private void TakeHitAndKnockback(Vector2 hitPoint)
-    {
-        ApplyKnockback(hitPoint);
-    }
-
-    private void ApplyKnockback(Vector2 hitPoint)
-    {
-        Vector2 knockbackDirection = ((Vector2)transform.position - hitPoint).normalized;
-        rb.AddForce(knockbackDirection * knockbackForce, ForceMode2D.Impulse);
-    }
-
-    private void ResetKnockback()
-    {
-        isKnockedBack = false;
-        rb.velocity = Vector2.zero; 
     }
 }
---
+++ Assets/Scripts/Diego/Vida.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Vida : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+    private Vector3 initialPosition;
+    public float knockbackForce = 10f;
+    private Rigidbody2D rb;
+    private bool isKnockedBack = false;
+
+    public Slider barraVid;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        initialPosition = transform.position;
+        rb = GetComponent<Rigidbody2D>();
+        initialPosition = transform.position;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            if (!isKnockedBack)
+            {
+                TakeHitAndKnockback(collision.contacts[0].point);
+                TakeDamage(20);
+                isKnockedBack = true;
+                Invoke("ResetKnockback", 0.5f);
+            }
+        }
+    }
+
+    private void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        currentHealth = Mathf.Max(currentHealth, 0);
+        barraVid.value = (float)currentHealth / maxHealth;
+
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void TakeHitAndKnockback(Vector2 hitPoint)
+    {
+        ApplyKnockback(hitPoint);
+    }
+
+    private void ApplyKnockback(Vector2 hitPoint)
+    {
+        Vector2 knockbackDirection = ((Vector2)transform.position - hitPoint).normalized;
+        rb.AddForce(knockbackDirection * knockbackForce, ForceMode2D.Impulse);
+    }
+
+    private void ResetKnockback()
+    {
+        isKnockedBack = false;
+        rb.velocity = Vector2.zero;
+    }
+}
